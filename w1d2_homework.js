7e207b8f3205e0c5cfb4bc316bedebe8bad10c74
jessiheger <jhmheger@gmail.com>
2018-03-20 16:18:22 -0700

    Commit 3 - the 3rd set of GIT homework answers are complete

--- w1d2_homework.js
+++ w1d2_homework.js
@@ -4,5 +4,10 @@
 // 2. git add
 // 3. git push
 
-//TEST
+// 1. git pull origin master
+// 2. git reset
+// 3. git checkout -- [FILENAME.TXT]
+// 4. git checkout -- allows you to "undo" the previous commits made to that file
+// 5. in  case you made a mistake in your last commit
+
 
