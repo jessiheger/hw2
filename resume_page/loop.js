1297d693e7055d9573df710a2b31577b64c0e2a6
jessiheger <jhmheger@gmail.com>
2018-03-20 17:31:06 -0700

    commit 7 - added initial styling for resume site

---
+++ resume_page/loop.js
@@ -0,0 +1,58 @@
+// 4. loop that logs the EVEN numbers from 0-98 (ascending)
+
+function evenAsc() {
+for (let i = 0; i <=98; i++){
+	if (i % 2 === 0) {
+		console.log(i);
+	}
+}
+}
+
+evenAsc();
+
+// 5. loop that logs the EVEN numbers from 98-0 (descending)
+
+function evenDes() {
+	for (let i = 98; i >= 0; i--) {
+		if (i % 2 === 0){
+			console.log(i);
+		}
+	}
+}
+
+evenDes();
+
+// 6. create a loop that logs the numbers from 49-72 (ascending)
+
+function num72Asc() {
+	for (let i = 49; i <= 72; i++){
+		console.log(i);
+	}
+}
+
+num72Asc();
+
+// 7. loop that logs the numbers from 81-26 (descending)
+
+function num81Des() {
+	for (let i = 81; i >= 26; i--){
+		console.log(i)
+	}
+}
+
+num81Des();
+
+// 8. loop that logs the numbers from 3-90 that are multiples of 3 (ascending)
+
+function multThree() {
+	for (let i = 3; i <= 90; i++){
+		if (i % 3 === 0) {
+			console.log(i);
+		}
+	}
+}
+
+multThree();
+
+
+
